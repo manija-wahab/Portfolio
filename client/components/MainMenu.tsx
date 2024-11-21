16e973d703a5df772435f4df8f14dec892cc2706
Manija Wahab <nooryawahab@gmail.com>
2024-11-21 19:01:33 +1300

    compressed video

--- client/components/MainMenu.tsx
+++ client/components/MainMenu.tsx
@@ -136,12 +136,9 @@ const MainMenu = () => {
         loop
         id="bgVideo"
         className="backgroundVideo"
-        rel="preload"
+        preload="auto"
       >
-        <source
-          src="/images/Japan/tokyo-street-sakura-moewalls-com.mp4"
-          type="video/mp4"
-        />
+        <source src="/images/Japan/sakura.mp4" type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
     </div>
